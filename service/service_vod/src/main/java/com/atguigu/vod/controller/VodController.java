9bf852b376b6557bb0bd52a3c697b9389a620af4
yzt <yzt@qq.com>
2020-04-17 10:28:02 +0800

    视频上传到阿里云

---
+++ service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
@@ -0,0 +1,25 @@
+package com.atguigu.vod.controller;
+
+import com.atguigu.commonutils.StandardResult;
+import com.atguigu.vod.service.VodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/eduvod/video")
+@CrossOrigin
+public class VodController {
+
+    @Autowired
+    private VodService vodService;
+
+    @PostMapping("/uploadVideo")
+    public StandardResult uploadVideo(MultipartFile file){
+        String videoId = vodService.uploadVideoAly(file);
+        return StandardResult.ok().data("videoId",videoId);
+    }
+}
---
+++ service/service_vod/src/main/java/com/atguigu/vod/service/impl/VodServiceImpl.java
@@ -0,0 +1,38 @@
+package com.atguigu.vod.service.impl;
+
+import com.aliyun.vod.upload.impl.UploadVideoImpl;
+import com.aliyun.vod.upload.req.UploadStreamRequest;
+import com.aliyun.vod.upload.resp.UploadStreamResponse;
+import com.atguigu.vod.Utils.ConstantVodUtils;
+import com.atguigu.vod.service.VodService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class VodServiceImpl implements VodService {
+    @Override
+    public String uploadVideoAly(MultipartFile file){
+
+        try {
+            String fileName = file.getOriginalFilename();
+            String title  = fileName.substring(0,fileName.lastIndexOf("."));
+            InputStream inputStream = file.getInputStream();
+            UploadStreamRequest request = new UploadStreamRequest(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET, title, fileName, inputStream);
+            UploadVideoImpl uploader = new UploadVideoImpl();
+            UploadStreamResponse response = uploader.uploadStream(request);
+            String videoId = null;
+            if (response.isSuccess()) {
+                videoId = response.getVideoId();
+            } else { //如果设置回调URL无效，不影响视频上传，可以返回VideoId同时会返回错误码。其他情况上传失败时，VideoId为空，此时需要根据返回错误码分析具体错误原因
+                videoId = response.getVideoId();
+            }
+            return videoId;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
