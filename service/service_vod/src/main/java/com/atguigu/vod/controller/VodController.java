8bcc535875b1e00a32499a03837b6ebcf0cd2b08
yzt <yzt@qq.com>
2020-04-26 09:33:44 +0800

    前端整合阿里云播放器，获取播放凭证。

--- service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
@@ -1,10 +1,18 @@
 package com.atguigu.vod.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
 import com.atguigu.commonutils.StandardResult;
+import com.atguigu.servicebase.exceptionHandler.GuliException;
+import com.atguigu.vod.Utils.ConstantVodUtils;
+import com.atguigu.vod.Utils.InitVodClient;
 import com.atguigu.vod.service.VodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +49,26 @@ public class VodController {
     public StandardResult removeMoreAlyVideos(@RequestParam("videoIdList") List<String> videoIdList){
         vodService.removeMoreAlyVideos(videoIdList);
         return StandardResult.ok();
+    }
+
+    //整合阿里云视频播放器，获取视频凭证
+    @GetMapping("/getVideoAuth/{id}")
+    public StandardResult getVideoAuth(@PathVariable String id){
+
+        try {
+            //1、获取client
+            DefaultAcsClient client = InitVodClient.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建获取凭证的request和response
+            GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+            request.setVideoId(id);
+            //获取播放凭证并返回
+            GetVideoPlayAuthResponse response = client.getAcsResponse(request);
+            String playAuth = response.getPlayAuth();
+            return StandardResult.ok().data("playAuth",playAuth);
+        } catch (ClientException e) {
+            e.printStackTrace();
+            throw new GuliException(20001,"获取播放凭证失败");
+        }
+
     }
 }
